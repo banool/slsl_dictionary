7bcfa9fa79212de380af916e0e5cddd0e10cf340
Daniel Porteous <danielporteous1@gmail.com>
2023-02-23 10:17:11 +0000

    Add support for reading prod_secrets.json

--- backend/slsl_backend/secrets.py
+++ backend/slsl_backend/secrets.py
@@ -11,8 +11,6 @@ REQUIRED = [
     "admin_username",
     "admin_password",
     "admin_email",
-    "static_bucket",
-    "media_bucket",
 ]
 
 OTHER = [
@@ -23,22 +21,20 @@ OTHER = [
     "additional_allowed_hosts",
 ]
 
-try:
-    # Try secrets.json first.
-    with open("secrets.json", "r") as f:
-        secrets = json.load(f)
-except FileNotFoundError:
-    try:
-        # Then /secrets/secrets.json.
-        with open("/secrets/secrets.json", "r") as f:
+POSSIBLE_FILENAMES = ["/secrets/secrets.json", "prod_secrets.json", "secrets.json"]
+
+secrets = {}
+for fname in POSSIBLE_FILENAMES:
+    if os.path.exists(fname):
+        with open(fname, "r") as f:
             secrets = json.load(f)
-    except FileNotFoundError:
-        # Then just env vars.
-        secrets = {}
-        for secret in REQUIRED + OTHER:
-            value = os.environ.get(secret)
-            if value is not None:
-                secrets[secret] = value
+        break
+
+if not secrets:
+    for secret in REQUIRED + OTHER:
+        value = os.environ.get(secret)
+        if value is not None:
+            secrets[secret] = value
 
 invalid = []
 for key in REQUIRED:
@@ -47,13 +43,17 @@ for key in REQUIRED:
 
 if "sql_unix_socket" in secrets:
     if "sql_host" in secrets or "sql_port" in secrets:
-        raise RuntimeError("You can't set both a unix socket and a host/port for the DB connection")
+        raise RuntimeError(
+            "You can't set both a unix socket and a host/port for the DB connection"
+        )
 
 if "sql_host" in secrets and "sql_port" not in secrets:
     raise RuntimeError("You must set both a host and a port for the DB connection")
 
 if not ("sql_unix_socket" in secrets or "sql_host" in secrets):
-    raise RuntimeError("You must set a unix socket or a host/port for the DB connection")
+    raise RuntimeError(
+        "You must set a unix socket or a host/port for the DB connection"
+    )
 
 
 if invalid:

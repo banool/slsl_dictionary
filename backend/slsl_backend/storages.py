c204191274dd5b1728a94d8820a448f15776aea6
Daniel Porteous <daniel@dport.me>
2023-05-30 10:13:49 +0100

    Fix collectstatic

--- backend/slsl_backend/storages.py
+++ backend/slsl_backend/storages.py
@@ -3,8 +3,8 @@ from storages.backends.gcloud import GoogleCloudStorage
 from slsl_backend.secrets import secrets
 
 StaticStorage = lambda: GoogleCloudStorage(
-    bucket=secrets["admin_bucket_name"], location="static"
+    bucket_name=secrets["admin_bucket_name"], location="static"
 )
 MediaStorage = lambda: GoogleCloudStorage(
-    bucket=secrets["media_bucket_name"], location="media"
+    bucket_name=secrets["media_bucket_name"], location="media"
 )

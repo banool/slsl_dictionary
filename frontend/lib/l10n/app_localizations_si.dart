32e902ab098c2bd50caa9d5a8d5fd7aaa2305e1a
Daniel Porteous <daniel@dport.me>
2024-02-03 11:33:38 +0000

    Don't lint l10n dir

--- frontend/lib/l10n/app_localizations_si.dart
+++ frontend/lib/l10n/app_localizations_si.dart
@@ -2,7 +2,7 @@ import 'app_localizations.dart';
 
 /// The translations for Sinhala Sinhalese (`si`).
 class AppLocalizationsSi extends AppLocalizations {
-  AppLocalizationsSi([super.locale = 'si']);
+  AppLocalizationsSi([String locale = 'si']) : super(locale);
 
   @override
   String get appTitle => 'ශ්‍රී ලංකා සංඥා භාෂා ශබ්දකෝෂය';
--- frontend/lib/l10n/app_localizations_ta.dart
+++ frontend/lib/l10n/app_localizations_ta.dart
@@ -2,7 +2,7 @@ import 'app_localizations.dart';
 
 /// The translations for Tamil (`ta`).
 class AppLocalizationsTa extends AppLocalizations {
-  AppLocalizationsTa([super.locale = 'ta']);
+  AppLocalizationsTa([String locale = 'ta']) : super(locale);
 
   @override
   String get appTitle => 'இலங்கை சைகை மொழி அகராதி';
